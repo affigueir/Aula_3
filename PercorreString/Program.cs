3b9268801cb57edf5bae8bf4359c8440ee81cadc
affigueir <affigueir@gmail.com>
2022-06-29 09:30:57 +0100

    Add PercorreString

---
+++ PercorreString/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace PercorreString
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please type a sentence:\n");
+            string input = Console.ReadLine();
+
+            System.Console.Write("Please type a char: ");
+            string charInput = Console.ReadLine();
+            char letter = charInput[0];
+
+            for (int i = 0; i < input.Length; i++)
+            {
+                if (input[i] == letter)
+                    continue;
+                else
+                {
+                    System.Console.Write(input[i]);
+                }
+            }
+
+        }
+    }
+}
